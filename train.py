37a239050bf53177e452b70b19942534f4417dfa
kimkihoon0515 <tega1996@naver.com>
2022-11-01 09:36:27 +0900

    tracking uri 인자 추가

--- train.py
+++ train.py
@@ -165,4 +165,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     print(args)
     MLFLOW_TRACKING_URI = os.environ['MLFLOW_TRACKING_URI'] = 'http://127.0.0.1:5000'
-    train(args)
+    train(args,MLFLOW_TRACKING_URI)
